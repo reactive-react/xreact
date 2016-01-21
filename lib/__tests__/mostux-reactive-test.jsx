b331eee1fb12fd366de422c18ca3b3c7e6d750fa
Jichao Ouyang <oyanglulu@gmail.com>
2016-01-21 23:43:16 +0800

    using most.zip instead of merge

--- lib/__tests__/mostux-reactive-test.jsx
+++ lib/__tests__/mostux-reactive-test.jsx
@@ -2,7 +2,6 @@ jest.dontMock('../mostux.js');
 import React from 'react';
 import ReactDOM from 'react-dom';
 import TestUtils from 'react-addons-test-utils';
-import uuid from 'uuid';
 import most from 'most';
 let {default: Mostux} = require('../mostux');
 let {connect} = require('../mostux');
@@ -19,21 +18,15 @@ describe('mostux', () => {
       render(){
         return (
           <div>
-            <button className="btn-complete" onClick={e=>this.props.actions.done(e,1)} >complete</button>
-            <button className="btn-delete" onClick={e=>this.props.actions.remove(e,2)}>delete</button>
+            <button className="btn-complete" onClick={e=>this.props.actions.done(1)} >complete</button>
+            <button className="btn-add" onClick={e=>this.props.actions.add(1)} >add</button>
+            <button className="btn-minus" onClick={e=>this.props.actions.minus(1)} >minus</button>
+            <button className="btn-delete" onClick={e=>this.props.actions.remove(2)}>delete</button>
           </div>)
       }
     });
 
     let TodoList = React.createClass({
-      getInitialState(){
-        return {
-          todos: [
-            {id:1, text:'hehe', done:false},
-            {id:2, text:'heheda', done:false},
-          ]
-        }
-      },
       render(){
         let {todos} = this.props;
         let todoElements;
@@ -49,34 +42,50 @@ describe('mostux', () => {
       }
     });
 
-    let RxTodoList = connect(TodoList, function(state$, intent$){
+    let RxTodoList = connect(TodoList, function(prevState$, intent$){
       let done$ = intent$.filter(x=>x.type=='done');
       let delete$ = intent$.filter(x=>x.type=='remove');
-      let newTodos = state$
-                          .combine(({todos}, done)=>{
-                            debugger
-                            return {todos:todos.map(todo=>{
-                              if(todo.id==done.id){
-                                todo.done =! todo.done;
-                                return todo;
-                              }
-                              return todo;
-                            })}
-                          }, done$)
-                          .combine(({todos}, deleted)=>{
-                            return {todos:todos.filter(todo=>todo.id!=deleted.id)}
-                          }, delete$)
-                          .startWith({
-                            todos: [
-                              {id:1, text:'hehe', done:false},
-                              {id:2, text:'heheda', done:false},
-                            ]
-                          })
-        return {
-          done: (event, id)=>({type:'done', id, event}),
-          remove: (event, id)=>({type:'remove', id, event}),
-          state$: newTodos,
+      let newState$ = intent$.zip((intent,{todos})=>{
+        switch(intent.type){
+          case 'done': return todos.map(todo=>{
+            if(todo.id==intent.id){
+              todo.done =! todo.done;
+              return todo;
+            }
+            return todo;
+          });
+          case 'remove': return todos.filter(todo=>todo.id!=intent.id);
+          case 'minus': return todos.map(todo=>{
+            if(todo.id==intent.id){
+              todo.text--
+              return todo
+            }
+            return todo;
+          });
+          case 'add': return todos.map(todo=>{
+            if(todo.id==intent.id){
+              todo.text++
+              return todo
+            }
+            return todo;
+          })
         }
+      },prevState$)
+                             .map(todos=>({todos}))
+                             .startWith({
+                               todos: [
+                                 {id:1, text:5, done:false},
+                                 {id:2, text:'heheda', done:false},
+                               ]
+                             });
+      return {
+        done: (id)=>({type:'done', id}),
+        remove: (id)=>({type:'remove', id}),
+        add: (id)=>({type:'add', id}),
+        minus: (id)=>({type:'minus', id}),
+        state$: newState$,
+
+      }
     });
     beforeEach(()=>{
       todolist = TestUtils.renderIntoDocument(
@@ -92,9 +101,27 @@ describe('mostux', () => {
       TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(todolist, 'btn-complete'));
       jest.runAllTimers();
       expect(TestUtils.scryRenderedComponentsWithType(todolist, Todo)[0].props.todo.done).toBe(true);
+
+    });
+    it('click remove button will remove item', ()=> {
+      jest.runAllTimers();
+      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(todolist, 'btn-delete'));
       TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(todolist, 'btn-delete'));
       jest.runAllTimers();
+      jest.runAllTicks();
       expect(TestUtils.scryRenderedComponentsWithType(todolist, Todo).length).toBe(1);
+    });
+    it('click add and minus', ()=> {
+      jest.runAllTimers();
+      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(todolist, 'btn-minus'));
+      jest.runAllTimers();
+      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(todolist, 'btn-add'));
+      jest.runAllTimers();
+      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(todolist, 'btn-minus'));
+      jest.runAllTimers();
+      TestUtils.Simulate.click(TestUtils.findRenderedDOMComponentWithClass(todolist, 'btn-minus'));
+      jest.runAllTimers();
+      expect(TestUtils.scryRenderedComponentsWithType(todolist, Todo)[0].props.todo.text).toBe(3);
     });
   });
 })
