1769a89acd7db9317d6674c5c7245c23cfb7e3c3
Jichao Ouyang <oyanglulu@gmail.com>
2016-11-13 21:35:26 +0800

    update example type-n-search-with-undo

--- examples/type-n-search-with-undo/src/app.jsx
+++ examples/type-n-search-with-undo/src/app.jsx
@@ -2,7 +2,7 @@ import {connect} from '../../../lib/react-most'
 import Most from '../../../lib/react-most'
 import ReactDOM from 'react-dom'
 import React from 'react'
-import most from 'most'
+import * as most from 'most'
 import rest from 'rest'
 const GITHUB_SEARCH_API = 'https://api.github.com/search/repositories?q=';
 
