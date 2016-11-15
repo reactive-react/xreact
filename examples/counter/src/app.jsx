8217b0eefb9e75d4524f5e6de2663f0ce5c163f7
oyanglulu <oyanglulu@gmail.com>
2016-11-15 21:56:11 +0800

    format code

--- examples/counter/src/app.jsx
+++ examples/counter/src/app.jsx
@@ -1,40 +1,38 @@
 import React from 'react';
-import {render} from 'react-dom';
-import Most, {connect} from '../../../lib/react-most'
+import { render } from 'react-dom';
+import Most, { connect } from '../../../lib/react-most'
 
 const CounterView = props => (
-      <div>
-	<button onClick={props.actions.dec}-</button>
-	<span>{props.count}</span>
-        <button onClick={props.actions.inc}>+</button>
-      </div>
-    )
-  }
-})
+	<div>
+		<button onClick={props.actions.dec}>-</button>
+		<span>{props.count}</span>
+		<button onClick={props.actions.inc}>+</button>
+	</div>
+)
 
-CounterView.defaultProps = {count: 0};
+CounterView.defaultProps = { count: 0 };
 
-const reactify = connect((intent$)=>{
-    return {
-	sink$: intent$.map(intent => {
-	    switch(intent.type) {
-	    case 'inc':
-		return state => ({count: state.count+1});
-	    case 'dec':
-		return state => ({count: state.count-1});
-	    default:
-		return _ => _;
-	    }
-	}),
-	inc: () => ({type: 'inc'}),
-	dec: () => ({type: 'dec'}),
-    }
+const reactify = connect((intent$) => {
+	return {
+		sink$: intent$.map(intent => {
+			switch (intent.type) {
+				case 'inc':
+					return state => ({ count: state.count + 1 });
+				case 'dec':
+					return state => ({ count: state.count - 1 });
+				default:
+					return _ => _;
+			}
+		}),
+		inc: () => ({ type: 'inc' }),
+		dec: () => ({ type: 'dec' }),
+	}
 })
 
 const Counter = reactify(CounterView)
 
 render(
-  <Most>
-    <Counter />
-  </Most>
-  , document.getElementById('app'));
+	<Most>
+		<Counter />
+	</Most>
+	, document.getElementById('app'));
