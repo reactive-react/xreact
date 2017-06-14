f1ad8f78eb544b3e41b04d7571f1ee9ca7684df6
Jichao Ouyang <oyanglulu@gmail.com>
2017-06-14 22:47:54 +0800

    lift12 and chain

--- src/fantasy.ts
+++ src/fantasy.ts
@@ -4,15 +4,17 @@ import { isXcomponentClass, XOrReactComponent, x } from './x'
 import { extendXComponentClass, genXComponentClass } from './xclass'
 export class FantasyX<E extends HKTS, I, S> {
   plan: Plan<E, I, S>
-  x: (WrappedComponent: XOrReactComponent<E, I, S>) => XcomponentClass<E, I, S>
   constructor(plan: Plan<E, I, S>) {
     this.plan = plan
   }
   apply(WrappedComponent) {
     return x(this.plan)(WrappedComponent)
   }
-  map<B>(f: (plan: Plan<E, I, S>) => Plan<E, I, B>) {
+  map<A>(f: (plan: Plan<E, I, S>) => Plan<E, I, A>): FantasyX<E, I, A> {
     return new FantasyX(f(this.plan))
+  }
+  chain<A>(f: (plan: Plan<E, I, S>) => FantasyX<E, I, A>): FantasyX<E, I, A> {
+    return f(this.plan)
   }
 }
 
@@ -21,4 +23,12 @@ export function pure<E extends HKTS, I, S>(plan: Plan<E, I, S>): FantasyX<E, I, 
 }
 export function map<E extends HKTS, I, A, B>(f: (plan: Plan<E, I, A>) => Plan<E, I, B>, fa: FantasyX<E, I, A>): FantasyX<E, I, B> {
   return fa.map(f)
+}
+
+export function lift<E extends HKTS, I, A, B>(f: (plan: Plan<E, I, A>) => Plan<E, I, B>): (fa: FantasyX<E, I, A>) => FantasyX<E, I, B> {
+  return fa => fa.map(f)
+}
+
+export function lift2<E extends HKTS, I, A, B, C>(f: (plan1: Plan<E, I, A>, plan2: Plan<E, I, B>) => Plan<E, I, C>): (fa1: FantasyX<E, I, A>, fa2: FantasyX<E, I, B>) => FantasyX<E, I, C> {
+  return (fa1, fa2) => new FantasyX(f(fa1.plan, fa2.plan))
 }
