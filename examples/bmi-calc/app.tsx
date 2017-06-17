39d03979e9f2799fc2becb7e200dc3a1c2128916
Jichao Ouyang <oyanglulu@gmail.com>
2017-06-17 22:39:04 +0800

    refactory bmi calc

--- examples/bmi-calc/app.tsx
+++ examples/bmi-calc/app.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 import * as RX from '../../src/xs/rx'
-
 import X from '../../src/x'
 import { pure, liftCombine } from '../../src/fantasy'
 import { Actions } from '../../src/interfaces'
@@ -12,41 +11,40 @@ interface Change extends Event {
 type Intent = Change
 interface BMIState {
   bmi: string
+  health: string
 }
 interface BMIProps<I> extends BMIState {
   actions: Actions<I>;
 }
 const View: React.SFC<BMIProps<Intent>> = props => (
   <div>
-    <input type="number" name="weight" defaultValue="0" onChange={props.actions.fromEvent} />
-    <input type="number" name="height" defaultValue="0" onChange={props.actions.fromEvent} />
-    <span>{props.bmi}</span>
+    <label>
+      weight(kg):
+      <input type="number" name="weight" defaultValue="0" onChange={props.actions.fromEvent} />
+    </label>
+    <label>
+      height(m):
+      <input type="number" name="height" defaultValue="0" onChange={props.actions.fromEvent} />
+    </label>
+    <p>Your BMI is <b>{props.bmi}</b></p>
+    <p>which means you're <b>{props.health}</b></p>
   </div>
 )
-View.defaultProps = { bmi: '.......' }
+View.defaultProps = { bmi: '', health: '' }
 
 interface InputState {
   value: number
 }
-const weightx = pure<RX.URI, Intent, InputState>(
-  (intent$) => {
-    return {
-      update$: intent$.filter(i => i.type == 'change' && i.target.name == 'weight')
-        .map(i => parseFloat(i.target.value))
-        .map(value => (() => ({ value })))
-    }
+const planx = name => (intent$) => {
+  return {
+    update$: intent$.filter(i => i.type == 'change' && i.target.name == name)
+      .map(i => parseFloat(i.target.value))
+      .map(value => (() => ({ value })))
   }
-)
+}
+const weightx = pure<RX.URI, Intent, InputState>(planx('weight'))
 
-const heightx = pure<RX.URI, Intent, InputState>(
-  (intent$) => {
-    return {
-      update$: intent$.filter(i => i.type == 'change' && i.target.name == 'height')
-        .map(i => parseFloat(i.target.value))
-        .map(value => (() => ({ value })))
-    }
-  }
-)
+const heightx = pure<RX.URI, Intent, InputState>(planx('height'))
 
 const BMIx = liftCombine<RX.URI, Intent, InputState, InputState, BMIState>(
   (u1, u2) => ((state) => {
@@ -56,14 +54,23 @@ const BMIx = liftCombine<RX.URI, Intent, InputState, InputState, BMIState>(
     else if (bmi < 24.9) health = 'normal'
     else if (bmi < 30) health = 'Overweight'
     else if (bmi >= 30) health = 'Obese'
-    return { bmi: health }
+    return { bmi: bmi.toString(), health }
   }))
   (weightx, heightx)
 
 const BMI = BMIx.apply(View)
 
+// BMIx is compose from weightx and heightx
+
+// while weightx and heightx can be still use to create another component
+const Weight = weightx.apply(props => props.value ? <p>where height is {props.value} kg</p> : null)
+const Height = heightx.apply(props => props.value ? <p>and {props.value}m height</p> : null)
 render(
   <X x={RX}>
-    <BMI />
+    <form>
+      <BMI />
+      <Weight />
+      <Height />
+    </form>
   </X>
   , document.getElementById('app'));
