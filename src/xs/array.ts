8be8cdec3bacaf06cc9bdea63bbde49b188fad21
Jichao Ouyang <oyanglulu@gmail.com>
2017-10-28 17:31:59 +0800

    adding array as stream

---
+++ src/xs/array.ts
@@ -0,0 +1,69 @@
+import { Subscription, StreamOps } from './index'
+
+declare module '.' {
+  interface S_<A> {
+    'ArrayStream': Array<A>
+  }
+}
+
+declare module '../fantasy/typeclasses' {
+  interface _<A> {
+    'ArrayStream': Array<A>
+  }
+}
+
+StreamOps.prototype.empty = function() {
+  return []
+}
+
+StreamOps.prototype.just = function(a) {
+  return [a]
+}
+
+StreamOps.prototype.scan = function(f, base, fa) {
+  return fa.scan(f, base)
+}
+
+StreamOps.prototype.combine = function <A, C>(
+  f: (...a: any[]) => C,
+  ...v: any[]
+): Array<C> {
+  return f.call(null, v)
+}
+
+StreamOps.prototype.filter = function <A>(f: (a: A) => boolean, fa: Array<A>): Array<A> {
+  return fa.filter(f)
+}
+StreamOps.prototype.map = function <A, B>(f: (a: A) => B, fa: Array<A>): Array<B> {
+  return fa.map(f)
+}
+StreamOps.prototype.flatMap = function <A, B>(f: (a: A) => Array<B>, fa: Array<A>): Array<B> {
+  return fa.reduce((acc, a) => acc.concat(f(a)), [] as B[])
+}
+
+class Subject<T> extends Array<T> {
+  next(a: T) {
+    this.push(a)
+  }
+  complete() {
+  }
+}
+
+StreamOps.prototype.subject = function <A>() {
+  return new Subject()
+}
+
+StreamOps.prototype.subscribe = function <A>(fa: Array<A>, next: (v: A) => void, complete?: () => void) {
+  throw Error("you don't need to subscribe a Array, just iterate it")
+}
+
+StreamOps.prototype.merge = function <A, B>(a: Array<A>, b: Array<B>): Array<A | B> {
+  return (<any>a).concat(b)
+}
+
+StreamOps.prototype.fromPromise = function(p) {
+  if (p.then) {
+    throw Error("You're not using real Promise aren't you, expecting Id Monad")
+  }
+  return [p.valueOf()]
+}
